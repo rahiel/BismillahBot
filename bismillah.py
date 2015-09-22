1537033be74ce990352d9f9c8c6807118ecfbbed
rahiel <rahiel@protonmail.ch>
2015-09-22 13:49:35 +0200

    Add License

--- bismillah.py
+++ bismillah.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+###############################################################################
+# BismillahBot -- Explore the Noble Qur'an on Telegram                        #
+# Copyright (C) 2015  Rahiel Kasim                                            #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU Affero General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU Affero General Public License for more details.                         #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+###############################################################################
 import re
 from time import sleep
 
@@ -139,7 +156,9 @@ def serve(bot, update_id, data):
                 text = ("The English translation is by Imam Ahmed Raza from "
                         "tanzil.net/trans/. The audio is a recitation by "
                         "Shaykh Mahmoud Khalil al-Husary from everyayah.com. "
-                        "The tafsir is Tafsir al-Jalalayn from altafsir.com")
+                        "The tafsir is Tafsir al-Jalalayn from altafsir.com."
+                        "The source code of the bot is available at: "
+                        "https://github.com/rahiel/BismillahBot.")
             else:
                 text = "Invalid command"
             bot.sendMessage(chat_id=chat_id, text=text)
